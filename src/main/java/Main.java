b4e8d0c2a8b089dea3142a711531c83a62222458
AleksandrAstahov <spirtt1985@yandex.ru>
2022-03-10 14:10:01 +0300

    Change Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int startingScore = 100;
+        int startingScore = 200;
         int replenishment = 100_000;
 
         if (replenishment >= 1000) {
-            int bonus = replenishment / startingScore;
+            int bonus = replenishment / 100;
             int total = startingScore + replenishment + bonus;
             System.out.println("Клиент пополнил счет на " + replenishment + "р." + " - бонус равен " + bonus + "р." +
                     " Баланс счета - " + total + "р.");
